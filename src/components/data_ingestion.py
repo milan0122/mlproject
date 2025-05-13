fd374444aca5d6a8e193c4fb8d715884c590597a
Milan <dangi.milan46@gmail.com>
2025-05-13 17:52:20 +0545

    data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,1 +1,52 @@
-# This file used for collecting the data from the various source like database, online rep, etc and sent the data for transformation and training
+import os 
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+
+from dataclasses import dataclass
+
+
+# using this class we can map the path, later if we  had to change our path, instead of changing mulitple place , we can do from here
+@dataclass
+class DataIngestionConfig: # 
+    train_data_path:str=os.path.join('artifacts','train.csv')
+    test_data_path:str=os.path.join('artifacts','test.csv')
+    raw_data_path:str=os.path.join('artifacts','data.csv')
+    
+
+class DataIngestion:
+    def __init__(self) -> None:
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Data Ingestion started")
+        try:
+            df = pd.read_csv('notebook/data/stud.csv')
+            logging.info('Read the dataset as dataframe')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)# ensure that the artifacts/directory exists so file can be written
+
+            #saving the raw data
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            logging.info("Train test split initiated")
+            #train_test_split
+            train_set, test_set = train_test_split(df,test_size=0.2,random_state=42)
+            #saving the train_set
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            #saving the test_set
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info('Ingestion of the data is completed')
+
+            # return the path to train_test_files
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        
+        except:
+            raise CustomException(e,sys)
+if __name__ == '__main__':
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
